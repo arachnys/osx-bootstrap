ebbc53388a375b1198bfb1537db2bfdfa757607a
Harry Waye <harry@arachnys.com>
2017-04-23 19:13:04 +0100

    Use hazzadous github account instead of arachnys.
    I've moved to personal account as it's probably better placed there
    for now.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 dir="$HOME/src/arachnys/"
 mkdir -p $dir
 cd $dir
-git clone --recursive https://github.com/arachnys/osx-bootstrap.git
+git clone --recursive https://github.com/hazzadous/osx-bootstrap.git
 cd osx-bootstrap
 bash bootstrap.sh
